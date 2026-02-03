eb2288c861835d34a7f512965ccb3e152199821e
b4yrra <t.chinbayr@gmail.com>
2026-02-03 13:16:46 +0800

    a

--- src/components/ui/carouselBar.tsx
+++ src/components/ui/carouselBar.tsx
@@ -7,6 +7,8 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import { Star } from "lucide-react";
+import { Play } from "lucide-react";
 
 export const CarouselBar = () => {
   return (
@@ -15,11 +17,46 @@ export const CarouselBar = () => {
         <CarouselContent>
           {Array.from({ length: 5 }).map((_, index) => (
             <CarouselItem key={index}>
-              <div className="p-1">
+              <div>
                 <Card>
                   <CardContent>
-                    <div>
-                      <img src="/Wicked.png" alt="" className="w-full" />
+                    <div className="bg-[url(/Wicked.jpg)] bg-cover bg-no-repeat bg-center w-full max-md:h-100 max-xl:h-130 xl:h-150 flex items-center">
+                      <div className="">
+                        <div className="p-7">
+                          <div>
+                            <p className="text-[14px] text-white xl:text-[16px]">
+                              Now Playing:
+                            </p>
+                            <h1 className="text-[24px] font-semibold text-white xl:text-[36px]">
+                              Wicked
+                            </h1>
+                          </div>
+                          <div className="flex gap-2">
+                            <Star className=" text-yellow-400" />
+                            <h2 className="text-slate-600 text-[16px] xl:text-[16px]">
+                              <span className="font-semibold text-[18px] text-white xl:text-[18px]">
+                                6.9
+                              </span>
+                              /10
+                            </h2>
+                          </div>
+                        </div>
+                        <div className="px-7">
+                          <p className="text-white xl:w-120 max-md:w-100 md:w-110 text-[12px]">
+                            Elphaba, a misunderstood young woman because of her
+                            green skin, and Glinda, a popular girl, become
+                            friends at Shiz University in the Land of Oz. After
+                            an encounter with the Wonderful Wizard of Oz, their
+                            friendship reaches a crossroads.
+                          </p>
+                        </div>
+                        <div className="p-7">
+                          <button className="flex gap-2 px-4 py-2 rounded-lg text-[14px] font-medium bg-white text-black">
+                            <Play />
+                            Watch Trailer
+                          </button>
+                        </div>
+                      </div>
                     </div>
                   </CardContent>
                 </Card>
--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -4,7 +4,7 @@ import { Moon } from "lucide-react";
 
 export const Header = () => {
   return (
-    <div className="flex justify-between items-center h-14.75 px-5">
+    <div className="flex justify-between items-center h-14 px-5 py-5 pb-5 xl:px-20">
       <div className="flex gap-2 h-5 items-center">
         <Film className="h-5 w-5 text-indigo-700" />
         <div className="text-[16px] italic font-bold text-indigo-700">
@@ -14,11 +14,11 @@ export const Header = () => {
       <div className="max-md:hidden md:block">
         <div className="flex gap-3">
           <div>
-            <button className="border-2 border-[#E4E4E7] rounded-lg py-2 px-4">
+            <button className="border-2 border-[#E4E4E7] rounded-lg py-1 px-2">
               ▼ Genre
             </button>
           </div>
-          <div className="border-2 border-[#E4E4E7] flex px-3 items-center rounded-lg gap-3">
+          <div className="border-2 border-[#E4E4E7] flex px-1 items-center rounded-lg gap-3">
             <Search className="w-4 text-slate-500" />
             <input
               type="text"
